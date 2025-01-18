3f97b2b47a1caf60d29a569919f72fdafca46446
fruitsdrink <fruitsdrink@126.com>
2025-01-18 23:50:25 +0800

    博客分享&开源项目

---
+++ src/data/blog.ts
@@ -0,0 +1,20 @@
+export const blogItems = [
+  {
+    title: "zyyo的博客",
+    site: "https://zyyo.cc",
+    description: "个人博客",
+    image: import("@/assets/images/blog/zyyo.cc.png"),
+  },
+  {
+    title: "YIKE时光",
+    site: "https://huohuo90.com/home",
+    description: "个人博客",
+    image: import("@/assets/images/blog/huohuo90.com.png"),
+  },
+  {
+    title: "YYF的博客",
+    site: "https://yanyunfeng.com/",
+    description: "个人博客",
+    image: import("@/assets/images/blog/yanyunfeng.com.png"),
+  },
+];
